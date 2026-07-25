6b633f2bf97147e201fd21b4ef115c072365e20f
2nm6sbhgs4-star <eklof.niclas@icloud.com>
2026-07-26 00:07:44 +0200

    Lägg till synlig feedback-knapp i navbaren

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,20 +7,23 @@ function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">Byggio</Link>
-      {!loading && (
-        user ? (
-          <div className="user-menu">
-            {user.photoURL && <img src={user.photoURL} alt="" className="user-avatar" />}
-            <span className="user-name">{user.displayName}</span>
-            <Link to="/mina-projekt" className="nav-link">Mina projekt</Link>
-            <Link to="/bli-hantverkare" className="nav-link">Bli hantverkarpartner</Link>
-            <Link to="/hantverkarpanel" className="nav-link">Hantverkarpanel</Link>
-            <button className="nav-button" onClick={logout}>Logga ut</button>
-          </div>
-        ) : (
-          <button className="nav-button" onClick={loginWithGoogle}>Logga in</button>
-        )
-      )}
+      <div className="navbar-right">
+        <Link to="/feedback" className="feedback-link">💬 Ge feedback</Link>
+        {!loading && (
+          user ? (
+            <div className="user-menu">
+              {user.photoURL && <img src={user.photoURL} alt="" className="user-avatar" />}
+              <span className="user-name">{user.displayName}</span>
+              <Link to="/mina-projekt" className="nav-link">Mina projekt</Link>
+              <Link to="/bli-hantverkare" className="nav-link">Bli hantverkarpartner</Link>
+              <Link to="/hantverkarpanel" className="nav-link">Hantverkarpanel</Link>
+              <button className="nav-button" onClick={logout}>Logga ut</button>
+            </div>
+          ) : (
+            <button className="nav-button" onClick={loginWithGoogle}>Logga in</button>
+          )
+        )}
+      </div>
     </nav>
   )
 }
